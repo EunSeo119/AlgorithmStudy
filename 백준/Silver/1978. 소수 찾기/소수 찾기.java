ecba9a1fed558b4a2f01031f0c03771202945155
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2023-02-19 17:45:12 +0900

    [Silver V] Title: 소수 찾기, Time: 76 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -1,29 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 0;
 		
-		int N = sc.nextInt();
+		boolean[] bool = new boolean[1001];  // 2이상 1000이하의 자연수 중에서 소수를 찾을거다.
 		
-		int count = 0;
-		for(int i = 0; i < N; i++) {
-			int num = sc.nextInt();
-			boolean check = true;
-			if(num==1) {
-				continue;
-			}
-			
-			for(int j = 2; j <= Math.sqrt(num); j++) {
-				if(num%j==0) {
-					check = false;
-					break;
+		//에라토스테네스의 체. 소수면 false, 아니면 true
+		bool[0] = true;
+		bool[1] = true;
+		for(int i = 2; Math.pow(i, 2) < 1000; i++) {
+			if(bool[i] == false) {
+				for(int j = 2; i * j <= 1000; j++ ) {
+					bool[i*j] = true;
 				}
 			}
-			if(check) {
-				count++;
-			}
 		}
-		System.out.println(count);
+		//소수면 카운트를 1 증가시킴
+		for(int i = 0; i < N; i++) {
+			if( bool[Integer.parseInt(st.nextToken())] == false ) ++cnt;
+		}
+		
+		System.out.println(cnt);
 	}
 }
