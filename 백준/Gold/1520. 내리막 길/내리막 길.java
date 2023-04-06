c33130748ed5adfce5bb47cf177f4882e9ecf8ca
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2023-04-07 01:36:03 +0900

    [Gold III] Title: 내리막 길, Time: 336 ms, Memory: 40032 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int M, N;
+	static int[][] map, dp;
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+
+		map = new int[M+1][N+1];
+		dp = new int[M+1][N+1]; // (x, y)에서 도착점으로 가는 경로의 개수
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.fill(dp[i], -1);
+		}
+
+		System.out.println(dfs(1, 1));
+	}
+
+	public static int dfs(int x, int y) {
+		if (x == M && y == N) {
+			return 1;
+		}
+
+		if (dp[x][y] != -1) {
+			return dp[x][y];
+		}
+
+		dp[x][y] = 0; // 현재 위치에서 끝점까지 도달하는 경로의 개수를 0으로 초기화.
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx<1 || ny<1 || nx>M || ny>N) {
+				continue;
+			}
+			
+			// map[x][y]보다 map[nx][ny]가 높이가 더 낮다면
+			// map[nx][ny]에서 끝점까지 도달하는 경로의 개수를 더한다.
+			if (map[x][y] > map[nx][ny]) {
+				dp[x][y] += dfs(nx, ny);
+			}
+		}
+
+		return dp[x][y];
+	}
+}
