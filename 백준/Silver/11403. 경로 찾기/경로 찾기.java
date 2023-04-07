ee86381fca14569759a7f6bf9844055859e090e7
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2023-04-07 10:34:43 +0900

    [Silver I] Title: 경로 찾기, Time: 484 ms, Memory: 27788 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+	static final int INF = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] graph = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				graph[i][j] = sc.nextInt();
+			}
+		}
+		
+		// 플로이드-워셜 알고리즘 이용
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(graph[i][k]==1 && graph[k][j]==1) {
+						graph[i][j] = 1;
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(graph[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
