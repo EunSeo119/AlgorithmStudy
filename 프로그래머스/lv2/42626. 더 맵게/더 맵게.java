571e7e191a1a39e4160d2dfa89d18d17efaed8ad
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2023-09-05 01:02:29 +0900

    [level 2] Title: 더 맵게, Time: 2428.71 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,23 +1,6 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville) {
-            pq.offer(s);
-        }
-
-        while(pq.peek() < K) {
-            if(pq.size() < 2) {
-                return -1;
-            }
-            int A = pq.poll();
-            int B = pq.poll();
-
-            pq.offer(A+B*2);
-            answer++;
-        }
         return answer;
     }
 }
