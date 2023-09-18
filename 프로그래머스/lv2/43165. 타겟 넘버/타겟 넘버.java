553fc1f94206bb11f1cb87a362a2f349c88a0cf1
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2023-09-18 17:38:19 +0900

    [level 2] Title: 타겟 넘버, Time: 3.77 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,27 @@
+import java.util.*;
+
 class Solution {
-    static int N = 0;
-    static int[] nums;
-    static int result = 0;
-    static int t;
     public int solution(int[] numbers, int target) {
-        N = numbers.length;
-        nums = numbers;
-        t = target;
-        subset();
-        return result;
-    }
-    
-    private static void subset() {
-        for(int i = 0; i < (1<<N); i++){
-            int sum = 0;
-            for(int j = 0; j < N; j++){
-                if((i & (1<<j))!=0){
-                    sum -= nums[j];
-                } else {
-                    sum += nums[j];
-                }
-            }
-            if(sum == t){
-                result++;
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        
+        for(int i = 0; i < numbers.length; i++) {
+            int size = queue.size();
+            for(int j = 0; j < size; j++) {
+                int sum = queue.poll();
+            
+            queue.add(sum+numbers[i]);
+            queue.add(sum-numbers[i]);
             }
         }
+        
+        while(!queue.isEmpty()) {
+            if(queue.poll() == target) {
+                answer++;
+            }
+        }
+        
+        return answer;
     }
 }
