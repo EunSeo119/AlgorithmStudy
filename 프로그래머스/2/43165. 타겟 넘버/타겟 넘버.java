4801bf16f30639176b08b2b79bf1da0ea9ffc9a3
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-01-24 17:46:48 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,36 @@
 class Solution {
-    static int N = 0;
-    static int[] nums;
-    static int result = 0;
-    static int t;
-    public int solution(int[] numbers, int target) {
-        N = numbers.length;
-        nums = numbers;
-        t = target;
-        subset();
-        return result;
-    }
-    
-    private static void subset() {
+    // subset(부분조합)으로 풀기
+    public int solution2(int[] numbers, int target) {
+        int answer = 0;
+        int N = numbers.length;
+
         for(int i = 0; i < (1<<N); i++){
             int sum = 0;
             for(int j = 0; j < N; j++){
-                if((i & (1<<j))!=0){
-                    sum -= nums[j];
+                if((i & (1<<j)) != 0){
+                    sum -= numbers[j];
                 } else {
-                    sum += nums[j];
+                    sum += numbers[j];
                 }
             }
-            if(sum == t){
-                result++;
+            if(sum == target){
+                answer++;
             }
+        }
+        return answer;
+    }
+
+    // DFS로 풀기
+    public static int solution(int[] numbers, int target) {
+        int answer = dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    public static int dfs(int[] numbers, int target, int idx, int sum) {
+        if (idx == numbers.length) {
+            return sum == target ? 1 : 0;
+        } else {
+            return dfs(numbers, target, idx + 1, sum + numbers[idx]) + dfs(numbers, target, idx + 1, sum - numbers[idx]);
         }
     }
 }
