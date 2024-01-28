40dfc12c3f5aefa8eb7c169548ce7a351fcb3017
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-01-28 20:38:26 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     // queue를 이용하여 품
-    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+    public ArrayList<Integer> solution2(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
 
@@ -22,6 +22,34 @@ class Solution {
             answer.add(count);
         }
         
+        return answer;
+    }
+    
+    // for문 + 이중 while문을 -> 한번의 for 문으로 개선
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int i = 0; i < speeds.length; i++) {
+            double remain = (100 - progresses[i]) / (double) speeds[i];
+            int date = (int) Math.ceil(remain);
+
+            if (!q.isEmpty() && q.peek() < date) {
+                answerList.add(q.size());
+                q.clear();
+            }
+
+            q.offer(date);
+        }
+
+        answerList.add(q.size());
+
+        int[] answer = new int[answerList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
         return answer;
     }
 }
