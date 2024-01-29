f2216ac2a967c3d96fa9322706d2072e680f0c9d
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-01-29 23:47:10 +0900

    [level 2] Title: 전화번호 목록, Time: 262.33 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     // HashSet의 contains를 이용하여 품
-    public boolean solution(String[] phone_book) {
+    public boolean solution2(String[] phone_book) {
         HashSet<String> set = new HashSet<>();
         for(String phone : phone_book) {
             set.add(phone);
@@ -13,6 +13,19 @@ class Solution {
                 if(set.contains(phone.substring(0, i))) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+    
+    // 조금 더 개선한 코드
+    public boolean solution(String[] phoneBook) {
+        // 정렬을 하면 접두어가 같으면 앞뒤로 단어가 정렬되게 된다.
+        Arrays.sort(phoneBook);
+        for (int i = 0; i < phoneBook.length-1; i++) {
+            // 하여 정렬 후 앞 단어가 뒤 단어에 포함이 되나만 확인하면 된다.
+            if (phoneBook[i+1].startsWith(phoneBook[i])) {
+                return false;
             }
         }
         return true;
