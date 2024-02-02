5ab43304f8d16d85b34a1b8cbd90cd313aa9ca2f
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-02-03 00:09:53 +0900

    [level 2] Title: 소수 찾기, Time: 13.34 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,20 +7,20 @@ class Solution {
         visited = new boolean[numbers.length()];
         numList = new ArrayList<>();
         for(int i = 1; i <= numbers.length(); i++){
-            int[] output = new int[i];
-	            perm(0, i, numbers, output);
+            char[] output = new char[i];
+            perm(0, i, numbers, output);
         }
         return numList.size();
     }
-    
+
     // 순열을 이용하여 숫자의 조합을 구하고
-    public void perm(int cnt, int N, String numbers, int[] output){
+    public void perm(int cnt, int N, String numbers, char[] output){
         if(cnt == N){
-            String[] strArr = new String[N];
-            for (int i = 0; i < N; i++) {
-                strArr[i] = String.valueOf(output[i]);
-            } 
-            int num = Integer.parseInt(String.join("", strArr));
+            String check = "";
+            for(int i = 0; i < N; i++) {
+                check += output[i];
+            }
+            int num = Integer.parseInt(check);
             // 해당 숫자의 조합이 소수인지 체크
             if(isPrime(num)){
                 if(!numList.contains(num)) numList.add(num);
@@ -29,13 +29,14 @@ class Solution {
         }
         for(int i = 0; i < numbers.length(); i++){
             if(visited[i]) continue;
+
             visited[i] = true;
-            output[cnt] = numbers.charAt(i) - '0';
+            output[cnt] = numbers.charAt(i);
             perm(cnt + 1, N, numbers, output);
             visited[i] = false;
         }
     }
-    
+
     public boolean isPrime(int n){
         if(n < 2) return false;
         for(int i = 2; i*i <= n; i++){
