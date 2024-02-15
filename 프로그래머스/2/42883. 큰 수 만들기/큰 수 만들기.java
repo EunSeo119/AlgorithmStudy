7592b15732f2ec8921d122d5cc146e486473e341
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-02-15 23:18:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.25 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 class Solution {
     // 조합을 이용하여 완전 탐색을 하면 런타임에러가남! -> Greedy 알고리즘을 이용해야함
-    public String solution(String number, int k) {
+    public String solution2(String number, int k) {
         StringBuilder answer = new StringBuilder();
 
         int start = 0;
@@ -15,5 +17,23 @@ class Solution {
             answer.append(max);
         }
         return answer.toString();
+    }
+    
+    // stack을 이용해서 풀수도 있다!
+    public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
     }
 }
