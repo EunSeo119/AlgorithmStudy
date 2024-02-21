aeb2cab2b4c38cb2ee6b55c4b79e58d6f70db0d0
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-02-21 23:55:50 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,5 +1,6 @@
+import java.util.*;
 class Solution {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+    public int[] solution2(String[] name, int[] yearning, String[][] photo) {
         int[] answer=new int[photo.length];
 
         // 3중 for문으로 한번에 photo의 2차원 배열을 돌면서 하나씩 name 배열 포함 유무를 확인
@@ -10,6 +11,33 @@ class Solution {
                         answer[i] += yearning[k];
                 }
             }
+        }
+        return answer;
+    }
+    
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        // 시간 단축을 위한 map 사용
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            String[] persons = photo[i];
+            int score = 0;
+
+            for (int j = 0; j < persons.length; j++) {
+                String person = persons[j];
+
+                if (map.containsKey(person)) {
+                    score += map.get(person);
+                }
+
+            }
+            answer[i] = score;
         }
         return answer;
     }
