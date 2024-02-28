558ec32bdf49880bbd37094d1e0d72ce903bdff0
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-02-28 23:47:27 +0900

    [level 2] Title: 모음 사전, Time: 0.11 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -61,7 +61,7 @@ class Solution3 {
 }
 
 // Solution3에 나눠져있는 재귀호출을 반복문으로 한번에 처리
-class Solution {
+class Solution4 {
     List<String> list = new ArrayList<>();
     public int solution(String word) {
         dfs("", 0);
@@ -76,5 +76,34 @@ class Solution {
         for(int i = 0; i < 5; i++) {
             dfs(str + "AEIOU".charAt(i), len + 1);
         }
+    }
+}
+
+/* 재귀호출(dfs)을 이용하지 않고 수학을 사용한 풀이
+f(0) = 1
+f(x) = f(x - 1) + 5^x
+f(1) = 1 + 5^1 = 6
+f(2) = 6 + 5^2 = 31
+f(3) = 31 + 5^3 = 156
+f(4) = 156 + 5^4 = 781
+=> int[] increase = {781, 156, 31, 6, 1};
+ */
+class Solution {
+    public int solution(String word) {
+        HashMap<Character, Integer> alphabet = new HashMap<>();
+        alphabet.put('A', 0);
+        alphabet.put('E', 1);
+        alphabet.put('I', 2);
+        alphabet.put('O', 3);
+        alphabet.put('U', 4);
+        int[] increase = new int[] {781, 156, 31, 6, 1};
+        
+        int answer = word.length();
+        
+        for(int i = 0; i < word.length(); i++){
+            answer += (increase[i] * alphabet.get(word.charAt(i)));
+        }
+        
+        return answer;
     }
 }
