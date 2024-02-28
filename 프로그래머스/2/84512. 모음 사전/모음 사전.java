982b744bb2f895b0eec4941aba4ff19696e8188d
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-02-28 23:31:59 +0900

    [level 2] Title: 모음 사전, Time: 2.86 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+class Solution2 {
     static String[] arr;
     static List<String> list;
     public int solution(String word) {
@@ -32,5 +32,30 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             dfs(word, str + arr[i], depth + 1);
         }
+    }
+}
+
+class Solution {
+    static int idx = 0;
+    static int answer = -1;
+    public int solution(String word) {
+        dfs(word, "");
+        return answer;
+    }
+    public void dfs(String word, String text) {
+        if(answer > 0) return;
+        if(word.equals(text)) {
+            answer=idx;
+        }
+        idx++;
+        if(text.length()==5) {
+            return;
+        }
+
+        dfs(word, text+"A");
+        dfs(word, text+"E");
+        dfs(word, text+"I");
+        dfs(word, text+"O");
+        dfs(word, text+"U");
     }
 }
