f4e0a5b77c98329ae0ecbd122893660df02c2299
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-04-19 00:46:13 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,27 @@
 import java.util.*;
 class Solution {
+    static boolean visited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
-        Queue<int[]> queue = new LinkedList<>();
+        visited = new boolean[n];
         
-        for(int i = 0; i < n; i++) {
-            if(visited[i]) continue;
-            
-            queue.offer(computers[i]);
-            visited[i] = true;
-            
-            while(!queue.isEmpty()) {
-                int[] computer = queue.poll();
-                
-                for(int j = 0; j < n; j++) {
-                    if(visited[j] || computer[j] == 0) continue;
-
-                    queue.offer(computers[j]);
-                    visited[j] = true;
-                }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers);
+                answer++;
             }
-            
-            answer++;
         }
         
         return answer;
+    }
+    
+    private static void dfs(int x, int[][] computers) {
+        visited[x] = true;
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (visited[i] || computers[x][i] == 0) continue;
+            
+            dfs(i, computers);
+        }
     }
 }
