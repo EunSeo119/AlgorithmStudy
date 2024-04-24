8c6dd936f4f1e7aafb9a2819ac053de1c94c9fe8
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-04-24 22:16:02 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.48 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,6 +2,24 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
+        
+        String[] numbers = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < numbers.length; i++){
+            int number = Integer.parseInt(numbers[i]);
+            
+            min = Math.min(min, number);
+            max = Math.max(max, number);
+        }
+
+        answer = min+ " " +max;
+        return answer;
+    }
+    
+    public String solution2(String s) {
+        String answer = "";
         String[] strArr = s.split(" ");
         int[] intArr = new int[strArr.length];
         
