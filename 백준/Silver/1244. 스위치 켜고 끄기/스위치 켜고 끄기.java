d3f313e4dd8d4460a2f148ff2c3a5fd78f8e9e78
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-05-28 02:43:20 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,42 +1,47 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int s_num = sc.nextInt();
-		int[] button = new int[s_num];
-		for (int i = 0; i < s_num; i++) {
-			button[i] = sc.nextInt();
-		}
-		int student = sc.nextInt();
-		for (int i = 0; i < student; i++) {
-			int gender = sc.nextInt();
-			int num = sc.nextInt();
+        int switchNum = Integer.parseInt(br.readLine());
+        int[] arr = new int[switchNum+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			if (gender == 1) {
-				for (int j = 0; j < s_num; j++) {
-					if ((j + 1) % num == 0) {
-						button[j] = button[j] == 1 ? 0 : 1;
-					}
-				}
-			} else if (gender == 2) {
-				for (int j = 0; j < Math.min(num, s_num-num); j++) {
-					if (num - j - 2 >= 0 && num + j < s_num && button[num - j - 2] == button[num + j]) {
-						button[num - j - 2] = button[num - j - 2] == 1 ? 0 : 1;
-						button[num + j] = button[num + j] == 1 ? 0 : 1;
-					} else {
-						break;
-					}
-				}
-				button[num - 1] = button[num - 1] == 1 ? 0 : 1;
-			}
-		}
-		for (int i = 1; i <= s_num; i++) {
-			System.out.print(button[i-1] + " ");
-			if (i % 20 == 0) {
-				System.out.println();
-			}
-		}
-	}
+        for(int i = 1; i <= switchNum; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int stdNum = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < stdNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gender = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+
+            if(gender == 1) {
+                for(int j = num; j <= switchNum; j+=num) {
+                    arr[j] = (arr[j] == 0) ? 1 : 0;
+                }
+            } else {
+                arr[num] = (arr[num] == 0) ? 1 : 0;
+
+                int count = 1;
+                while(num - count >= 1 && num + count <= switchNum && arr[num-count] == arr[num+count]) {
+                    arr[num-count] = (arr[num-count] == 0) ? 1 : 0;
+                    arr[num+count] = (arr[num+count] == 0) ? 1 : 0;
+                    count++;
+                }
+            }
+        }
+
+        for(int i = 1; i <= switchNum; i++) {
+            System.out.print(arr[i]+" ");
+            if(i%20 == 0) {
+                System.out.println();
+            }
+        }
+
+    }
 }
