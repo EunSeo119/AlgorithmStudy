e41d78248727cc592d6cacfdd74eb8f03c503a77
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-05-29 19:32:24 +0900

    [D3] Title: 햄버거 다이어트, Time: 985 ms, Memory: 27,500 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,43 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		int N = 0, L = 0;
-		for(int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
+    static int[][] score;
+    static int result;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
 
-			int[][] data = new int[N][2];
-			int result = 0;
-			
-			for(int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				data[i][0] = Integer.parseInt(st.nextToken());
-				data[i][1] = Integer.parseInt(st.nextToken());
-			}
-			
-			for(int i = 0; i < (1<<N); i++) {
-				int taste = 0;
-				int kcal = 0;
-				for(int j = 0; j < N; j++) {
-					if((i & (1<<j))!=0) {
-						taste += data[j][0];
-						kcal += data[j][1];
-					}
-				}
-				if(kcal <= L) {
-					result = Math.max(result, taste);
-				}
-			}
-			System.out.println("#"+t+" "+result);
-		}
-	}
+        for(int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            score = new int[N][2];
+            result = 0;
+
+            for(int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                score[i][0] = Integer.parseInt(st.nextToken());
+                score[i][1] = Integer.parseInt(st.nextToken());
+            }
+
+            subset(N, L);
+
+            System.out.println("#" + t + " " + result);
+        }
+    }
+
+    private static void subset(int N, int L) {
+        for(int i = 0; i < (1<<N); i++) {
+            int taste = 0;
+            int kcal = 0;
+            for(int j = 0; j < N; j++) {
+                if((i & (1<<j)) == 0) continue;
+
+                taste += score[j][0];
+                kcal += score[j][1];
+            }
+
+            if(kcal <= L) {
+                result = Math.max(taste, result);
+            }
+        }
+    }
 }
