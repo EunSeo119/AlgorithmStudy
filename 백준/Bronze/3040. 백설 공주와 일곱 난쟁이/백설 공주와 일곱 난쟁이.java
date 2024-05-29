563f2d8f8008797d8cf1fc9884241bf3f18434d9
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-05-29 17:36:03 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 80 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,53 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-//import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-	static int N = 9;
-	static int M = 7;
-	static int[] inputs, numbers;
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static StringTokenizer st;
-	public static void main(String[] args) throws IOException {
-//		Scanner sc = new Scanner(System.in);
-		br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		inputs = new int[N];
-		numbers = new int[M];
-		for(int i = 0; i < N; i++) {
-//			inputs[i] = sc.nextInt();
-			inputs[i] = Integer.parseInt(br.readLine());
-		}
-		
-		comb(0, 0);
-		
-		bw.flush();
-		bw.close();
-	}
-	private static void comb(int cnt, int start) throws IOException {
-		if(cnt == M) {
-			int sum = 0;
-			for(int i = 0; i < M; i++) {
-				sum+=numbers[i];
-			}
-			if(sum==100) {
-				for(int j = 0; j < M; j++) {
-//					System.out.println(numbers[j]);
-					bw.write(numbers[j]+"\n");
-				}
-			}
-			return;
-		}
-		
-		for(int i = start; i < N; i++) {
-			numbers[cnt] = inputs[i];
-			comb(cnt+1, i+1);
-		}
-	}
+    static int N = 9;
+    static int R = 7;
+    static int[] numbers, result;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        numbers = new int[9];
+        result = new int[7];
+
+        for(int i = 0; i < 9; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        comb(0, 0);
+    }
+
+    private static void comb(int start, int cnt) {
+        if(cnt == R) {
+            int sum = 0;
+            for(int i = 0; i < 7; i++) {
+                sum += result[i];
+            }
+
+            if(sum == 100) {
+                for(int i = 0; i < 7; i++) {
+                    System.out.println(result[i]);
+                }
+            }
+
+            return;
+        }
+
+        for(int i = start; i < N; i++) {
+            result[cnt] = numbers[i];
+            comb(i+1, cnt+1);
+        }
+    }
 }
