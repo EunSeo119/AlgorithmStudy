261c3dce7e9299d9fcb8dd5db0a8d7c56cc92780
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-06-19 21:24:04 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,45 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * 부분집합을 사용해서 모든 재료 조합을 구함
+ * 부분집합은 비트마스크를 사용해서 구현함
+ */
 public class Main {
-	static int N;
-	static int[] inputS, inputB;
-	static int result;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		inputS = new int[N];
-		inputB = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			inputS[i] = sc.nextInt();
-			inputB[i] = sc.nextInt();
-		}
-		
-		int tmpS = 0;
-		int tmpB = 0;
-		for(int i = 0; i < N; i++) {
-			tmpS*=inputS[i];
-			tmpB+=inputB[i];
-		}
-		result = Math.max(tmpS, tmpB);
-		
-		subset();
-		System.out.println(result);
-	}
-	private static void subset() {
-		int S;
-		int B;
-		for (int i = 1; i < (1<<N); ++i) {
-			S = 1;
-			B = 0;
-			for(int j = 0; j < N; ++j) {
-				if((i & (1 << j))==0) continue;
-				
-				S*=inputS[j];
-				B+=inputB[j];
-			}
-			
-			result = Math.min(result, Math.abs(S-B));
-		}
-	}
+    static int[][] food;
+    static int N, result;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        food = new int[N][2];
+        result = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            food[i][0] = Integer.parseInt(st.nextToken());
+            food[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        subset();
+
+        System.out.println(result);
+    }
+
+    private static void subset() {
+        int combS, combB;
+        for(int i = 1; i < (1<<N); i++) {
+            combS = 1;
+            combB = 0;
+            for(int j = 0; j < N; j++) {
+                if((i&(1<<j)) == 0) continue;
+
+                combS *= food[j][0];
+                combB += food[j][1];
+            }
+
+            result = Math.min(result, Math.abs(combS - combB));
+        }
+    }
 }
