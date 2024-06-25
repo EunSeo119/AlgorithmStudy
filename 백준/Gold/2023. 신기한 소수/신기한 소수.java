6e058c857b9863eacb8a8d28215c2bfa36adb8cb
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-06-25 17:06:10 +0900

    [Gold V] Title: 신기한 소수, Time: 72 ms, Memory: 11424 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    static int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    static int N;
+    static int result;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        result = 0;
+
+        perm(0);
+    }
+
+    private static void perm(int cnt) {
+        if(cnt == N) {
+            System.out.println(result);
+            return;
+        }
+
+        for(int i = 0; i < 10; i++) {
+            int check = result*10 + nums[i];
+            if(isPrime(check)) {
+                result = check;
+                perm(cnt+1);
+                result /= 10;
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if(num < 2) return false;
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num%i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
