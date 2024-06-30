b07490e305fef45991b4bd2d59a5165fbebbd771
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-06-30 20:37:36 +0900

    [Gold V] Title: 탑, Time: 664 ms, Memory: 107848 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * 풀이: for문 구현은 시간 초과 -> stack 사용
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i = 0; i < N; i++) {
+            int h = Integer.parseInt(st.nextToken());
+
+            while(true) {
+                if(stack.isEmpty()) {
+                    sb.append(0).append(" ");
+                    break;
+                } else if(stack.peek()[0] > h) {
+                    sb.append(stack.peek()[1]).append(" ");
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+
+            stack.add(new int[] {h, i+1});
+        }
+
+        System.out.println(sb);
+    }
+}
