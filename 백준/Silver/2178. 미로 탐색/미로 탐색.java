52fcfdae343c5e98ded76428788073563ee749f4
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-07-06 23:55:08 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/**
+ * 풀이: bfs 사용, depth 1씩 늘려서 큐에 배열로 저장
+ */
 public class Main {
-	static int[][] map;
-	static int N, M;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static int result = 1;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j)-'0'; 
-			}
-		}
-		
-		bfs();
-		System.out.println(result);
-	}
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int N, M;
+    static char[][] graph;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static void bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {0, 0, 1});
-		map[0][0] = 0;
-		
-		while(!q.isEmpty()) {
-			int[] current = q.poll();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-			for(int i = 0; i < 4; i++) {
-				int nx = dx[i] + current[0];
-				int ny = dy[i] + current[1];
-				if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-				if(nx==N-1 && ny==M-1) {
-					result = current[2]+1;
-					return;
-				}
-				if(map[nx][ny]==1) {
-					q.offer(new int[] {nx, ny, current[2]+1});
-					map[nx][ny] = 0;
-				}
-			}
-		}
-	}
+        graph = new char[N][M];
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                graph[i][j] = str.charAt(j);
+            }
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0, 0, 0});
+
+        int[] current = new int[3];
+        while(!q.isEmpty()) {
+            current = q.poll();
+
+            if(current[0] == N-1 && current[1] == M-1) break;
+
+            for(int i = 0; i < 4; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+                if(graph[nx][ny] == '1') {
+                    graph[nx][ny] = '0';
+                    q.offer(new int[] {nx, ny, current[2]+1});
+                }
+            }
+        }
+
+        System.out.println(current[2]+1);
+    }
 }
