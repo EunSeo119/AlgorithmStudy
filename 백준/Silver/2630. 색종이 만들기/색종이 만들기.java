3441bdd2ff413b56138228ee600db4a99f7e962b
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-07-14 14:24:05 +0900

    [Silver II] Title: 색종이 만들기, Time: 104 ms, Memory: 13140 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,48 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 풀이: 재귀와 분할 정복 사용
+ */
 public class Main {
-	static int[] answer;
-	static int[][] map;
+    static int[][] graph;
+    static int blue, white;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        blue = 0;
+        white = 0;
 
-		int N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		answer = new int[2];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		cut(0, 0, N);
-		System.out.println(answer[0]);
-		System.out.println(answer[1]);
-	}
+        graph = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-	private static void cut(int start, int end, int size) {
-		int sum = 0;
-		for (int i = start; i < start + size; i++) {
-			for (int j = end; j < end + size; j++) {
-				sum += map[i][j];
-			}
-		}
-		if (sum == 0) {
-			answer[0]++;
-			return;
-		} else if (sum == size * size) {
-			answer[1]++;
-			return;
-		}
-		int half = size / 2;
-		cut(start, end, half);
-		cut(start + half, end, half);
-		cut(start, end + half, half);
-		cut(start + half, end + half, half);
+        cut(0, 0, N);
 
-	}
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    private static void cut(int row, int col, int size) {
+        int sum = 0;
+        for(int i = row; i < row+size; i++) {
+            for(int j = col; j < col+size; j++) {
+                if(graph[i][j] == 1) {
+                    sum++;
+                    continue;
+                }
+            }
+        }
+
+        if(sum == (size*size)) {
+            blue++;
+            return;
+        }
+
+        if(sum == 0) {
+            white++;
+            return;
+        }
+
+        cut(row, col, size/2);
+        cut(row, col+size/2, size/2);
+        cut(row+size/2, col, size/2);
+        cut(row+size/2, col+size/2, size/2);
+    }
 }
