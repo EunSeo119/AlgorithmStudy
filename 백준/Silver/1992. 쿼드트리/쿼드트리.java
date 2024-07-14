007a5b500efab2dce12236e437d395d23e47cdac
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-07-14 14:59:47 +0900

    [Silver I] Title: 쿼드트리, Time: 80 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,61 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-/*
-8
-00000000
-00000000
-00001111
-00001111
-00011111
-00111111
-00111111
-00111111
+
+/**
+ * 풀이: 백준 '색종이 만들기'와 유사, 재귀와 분할정복 사용, 재귀 사용 순서 고려 + 괄호 고려
  */
 public class Main {
-	static int[][] spaces;
-	
-	static void cut(int r, int c, int size) {
+    static int[][] graph;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int sum = 0;
-		for(int i = r, rEnd = r+size; i < rEnd; i++) {
-			for(int j = c, cEnd = c+size; j < cEnd; j++) {
-				sum += spaces[i][j];
-			}
-		}
-		
-		if(sum == size * size) {
-			System.out.print(1);
-			return;
-		} else if(sum == 0) {
-			System.out.print(0);
-			return;
-		} else {	// 혼합된 상황
-			// 4분할
-			int half = size/2;
-			System.out.print("(");
-			cut(r, c, half);
-			cut(r, c+half, half);
-			cut(r+half, c, half);
-			cut(r+half, c+half, half);
-			System.out.print(")");
-		}
-		
-		
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		spaces = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < N; j++) {
-				spaces[i][j] = str.charAt(j)-'0';
-			}
-		}
-		
-		cut(0,0,N);
-	}
+        int N = Integer.parseInt(br.readLine());
+
+        graph = new int[N][N];
+        sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < N; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        cut(0, 0, N);
+
+        System.out.println(sb);
+    }
+
+    private static void cut(int row, int col, int size) {
+        int sum = 0;
+        for(int i = row; i < row+size; i++) {
+            for(int j = col; j < col+size; j++) {
+                if(graph[i][j] == 1) {
+                    sum++;
+                }
+            }
+        }
+
+        if(sum == (size*size)) {
+            sb.append(1);
+            return;
+        }
+
+        if(sum == 0) {
+            sb.append(0);
+            return;
+        }
+
+        sb.append("(");
+        cut(row, col, size/2);
+        cut(row, col+size/2, size/2);
+        cut(row+size/2, col, size/2);
+        cut(row+size/2, col+size/2, size/2);
+        sb.append(")");
+    }
 }
