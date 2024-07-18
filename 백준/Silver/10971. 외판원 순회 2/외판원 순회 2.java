9d32a55e8e36d059ca0d294353f7d6c0e99e6421
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-07-19 01:32:49 +0900

    [Silver II] Title: 외판원 순회 2, Time: 112 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,53 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * 풀이: dfs 사용(백트래킹)
+ */
 public class Main {
-	static int N;
-	static int[][] W;
-	static boolean[] visited;
-	static int[] number;
-	static int min = Integer.MAX_VALUE;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		W = new int[N][N];
-		visited = new boolean[N];
-		number = new int[N+1];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				W[i][j]=sc.nextInt();
-			}
-		}
-		
-		perm(0);
-		System.out.println(min);
-	}
+    static boolean[] visited;
+    static int N, result;
+    static int[][] city;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	private static void perm(int cnt) {
-		if(cnt==N) {
-			int sum = 0;
-			number[N] = number[0];
-			for(int i = 0; i < N; i++) {
-				int start = number[i];
-				int end = number[i+1];
-				if(W[start][end]==0) {
-					sum = Integer.MAX_VALUE;
-					break;
-				}
-				sum += W[start][end];
-			}
-			
-			min = min>sum?sum:min;
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if(visited[i])
-				continue;
-			
-			visited[i] = true;
-			number[cnt] = i;
-			perm(cnt+1);
-			visited[i] = false;
-		}
-	}
+        N = Integer.parseInt(br.readLine());
+
+        city = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                city[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited = new boolean[N];
+        result = Integer.MAX_VALUE;
+        for(int i = 0; i < N; i++) {
+            visited[i] = true;
+            dfs(0, 0, i, i);
+        }
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int depth, int sum, int start, int now) {
+        if(depth == N-1) {
+            if(city[now][start] != 0) {
+                result = Math.min(result, sum+city[now][start]);
+            }
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(visited[i] || city[now][i] == 0) continue;
+
+            visited[i] = true;
+            dfs(depth+1, sum+city[now][i], start, i);
+            visited[i] = false;
+        }
+
+    }
 }
