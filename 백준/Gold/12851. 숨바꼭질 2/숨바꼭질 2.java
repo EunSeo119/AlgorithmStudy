c004275839262c81c6967afaa2bc18da8e67912a
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-08-11 15:47:53 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 152 ms, Memory: 39768 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 풀이: bfs 사용
+ */
+public class Main {
+    static int[] move = {1, -1, 2};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // 수빈이와 동생의 위치가 같음
+        if(N == K) {
+            System.out.println(0);
+            System.out.println(1);
+            return;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+        int[] time = new int[100001];
+        int result = 0;
+
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for(int i = 0; i < 3; i++) {
+                int next;
+                if(i==2) {
+                    next = current * move[i]; // 순간이동
+                } else {
+                    next = current + move[i]; // 걷기
+                }
+
+                if(next < 0 || next > 100000 || (time[next] != 0 && time[next] < time[current]+1)) continue;
+
+                time[next] = time[current] + 1;
+                queue.offer(next);
+
+                if(next == K) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(time[K]);
+        System.out.println(result);
+    }
+}
