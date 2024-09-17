3c71d694828d47664fdfd23dcd1cec7057092049
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-09-18 00:08:31 +0900

    [level 3] Title: 여행경로, Time: 71.90 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+class Solution2 {
     static int len;
     static String[] result;
     static boolean[] visited;
@@ -49,5 +49,37 @@ class Solution {
         }
         
         return false;
+    }
+}
+
+class Solution {
+
+    static List<String> list = new ArrayList<>();
+    static boolean visited[];
+
+    public String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];
+
+        dfs(0, "ICN", "ICN", tickets);
+
+        // 모든 경우를 다해본 뒤 경로가 2개 이상일 경우 나중에 정렬을 하는 방법!
+        Collections.sort(list);
+
+        return list.get(0).split(" ");
+    }
+
+    static void dfs(int depth, String now, String path, String[][] tickets){
+        if (depth == tickets.length) {
+            list.add(path);
+            return;
+        }
+
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i] && now.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(depth+1, tickets[i][1], path + " " +tickets[i][1], tickets);
+                visited[i] = false;
+            }
+        }
     }
 }
