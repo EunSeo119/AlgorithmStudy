c3cd2193372dd25d4a1c1c673126d76b7f2c267a
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-09-19 22:04:26 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 12824 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,83 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
+/**
+ * 풀이: bfs 사용
+ */
 public class Main {
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static int count1 = 0;
-	static int count2 = 0;
-	static int N;
-	static String[][] grid;
-	static boolean[][] visited;
-	static class Pos {
-		int x, y;
-		String color;
+    static boolean[][] visited;
+    static char[][] picture1, picture2;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		public Pos(int x, int y, String color) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.color = color;
-		}
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		grid = new String[N][N];
-		visited = new boolean[N][N];
-		for(int i = 0; i < N; i++) {
-			String[] str = br.readLine().split("");
-			for(int j = 0; j < N; j++) {
-				grid[i][j] = str[j];
-			}
-		}
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					count1++;
-					dfs(i, j, grid[i][j]);
-				}
-			}
-		}
-		
-		visited = new boolean[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(grid[i][j].equals("G")) {
-					grid[i][j] = "R";
-				}
-			}
-		}
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					count2++;
-					dfs(i, j, grid[i][j]);
-				}
-			}
-		}
-		
-		System.out.print(count1 + " " +count2);
-	}
-	
-	private static void dfs(int x, int y, String color) {
-		visited[x][y] = true;
-		
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-			if(!grid[nx][ny].equals(color)) continue;
-			if(visited[nx][ny]) continue;
-			
-			dfs(nx, ny, color);
-		}
-	}
+        N = Integer.parseInt(br.readLine());
+
+        picture1 = new char[N][N];
+        picture2 = new char[N][N];
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < N; j++) {
+                picture1[i][j] = str.charAt(j);
+
+                if(picture1[i][j] == 'G') {
+                    picture2[i][j] = 'R';
+                    continue;
+                }
+
+                picture2[i][j] = picture1[i][j];
+            }
+        }
+
+        int result = 0;
+        for(int t = 0; t < 2; t++) {
+            visited = new boolean[N][N];
+            result = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(visited[i][j]) continue;
+
+                    visited[i][j] = true;
+                    if(t == 0) {    // 일반인
+                        bfs(new int[] {i, j}, picture1);
+                    } else {    // 적록색약
+                        bfs(new int[] {i, j}, picture2);
+                    }
+
+                    result++;
+                }
+            }
+            System.out.print(result + " ");
+        }
+    }
+
+    static private void bfs(int[] input, char[][] picture) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(input);
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+
+                if(picture[current[0]][current[1]] == picture[nx][ny]) {
+                    queue.offer(new int[] {nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+
+        }
+    }
 }
