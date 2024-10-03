714db1f5c7445fa5ae5ccc6663ff82a4b40637c0
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-10-03 22:48:01 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,57 +1,53 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
+/**
+ * 풀이: 조합 활용
+ */
 public class Main {
-	static int L, C;
-	static List<String> result;
-	static int count = 0;
-	static List<String> alpha;
-	static String[] str;
+    static int L, C;
+    static String[] alphabets, result;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-		L = sc.nextInt();
-		C = sc.nextInt();
+        alphabets = new String[C];
+        result = new String[L];
 
-		alpha = new ArrayList<>();
-		result = new ArrayList<>();
-		str = new String[L];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < C; i++) {
+            alphabets[i] = st.nextToken();
+        }
+        Arrays.sort(alphabets);
+        comb(0, 0);
+    }
 
-		for (int i = 0; i < C; i++) {
-			alpha.add(sc.next());
-		}
+    private static void comb(int start, int cnt) {
+        if(cnt == L) {
+            String str = "";
+            int moo = 0;
+            int zaa = 0;
+            for(int i = 0; i < L; i++) {
+                if(result[i].equals("a") || result[i].equals("e") || result[i].equals("i") || result[i].equals("o") || result[i].equals("u")) {
+                    moo++;
+                } else {
+                    zaa++;
+                }
+                str += result[i];
+            }
 
-		comb(0, 0, 0, 0);
-		Collections.sort(result);
-		for (int i = 0; i < result.size(); i++) {
-			System.out.println(result.get(i));
-		}
+            if(moo >= 1 && zaa >= 2) {
+                System.out.println(str);
+            }
+            return;
+        }
 
-	}
-
-	private static void comb(int cnt, int start, int moNum, int zaNum) {
-		if (cnt == L) {
-			if (moNum >= 1 && zaNum >= 2) {
-				String[] strCopy = Arrays.copyOf(str, str.length);
-				Arrays.sort(strCopy);
-				String s = String.join("", strCopy);
-				result.add(s);
-			}
-			return;
-		}
-
-		for (int i = start; i < C; i++) {
-			str[cnt] = alpha.get(i);
-			if (str[cnt].equals("a") || str[cnt].equals("e") || str[cnt].equals("i") || str[cnt].equals("o")
-					|| str[cnt].equals("u"))
-				comb(cnt + 1, i + 1, moNum + 1, zaNum);
-			else
-				comb(cnt + 1, i + 1, moNum, zaNum + 1);
-
-		}
-	}
+        for(int i = start; i < C; i++) {
+            result[cnt] = alphabets[i];
+            comb(i+1, cnt+1);
+        }
+    }
 }
