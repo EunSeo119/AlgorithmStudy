8afb01584cd9c179b18ab7191592ffae1ae0c7bd
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-11-05 16:19:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.61 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 52.5 MB, 시간: 3.76 ms
+메모리: 56.8 MB, 시간: 7.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 22일 23:24:03
+2024년 11월 05일 16:19:13
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,38 +4,32 @@ class Solution {
         int answer = 0;
         int[] dx = {0, 0, -1, 1};
         int[] dy = {1, -1, 0, 0};
-        int[][] visited = new int[maps.length][maps[0].length];
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         Queue<int[]> queue = new LinkedList<>();
         
-        visited[0][0] = 1;
-        queue.offer(new int[] {0, 0});
+        queue.offer(new int[] {0, 0, 0});
+        visited[0][0] = true;
         
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int depth = current[2]+1;
             
             for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
                 
-                // maps 벗어나는 경우 체크
-                if(nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1) {
-                    continue;
+                if(nx < 0 || ny < 0 || 
+                   nx >= maps.length || ny >= maps[0].length || 
+                   visited[nx][ny] || maps[nx][ny] == 0) continue;
+                
+                if(nx == maps.length-1 && ny == maps[0].length-1) {
+                    return depth+1;
                 }
                 
-                // 방문한 적이 없고 벽이 없는 자리일 시 queue에 추가
-                if(visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[x][y] + 1;
-                    queue.offer(new int[] {nx, ny});
-                }
+                queue.offer(new int[] {nx, ny, depth});
+                visited[nx][ny] = true;
             }
         }
-        
-        answer = visited[maps.length-1][maps[0].length-1];
-        
-        if(answer == 0) return -1;
-        
-        return answer;
+        return -1;
     }
 }
