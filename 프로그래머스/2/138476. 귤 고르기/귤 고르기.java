0290a776dd9251e9049b269f2b5dc6bf4e6eafea
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-11-05 14:10:59 +0900

    [level 2] Title: 귤 고르기, Time: 157.24 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,20 +4,20 @@ class Solution {
         int answer = 0;
         Arrays.sort(tangerine);
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         HashMap<Integer, Integer> hm = new HashMap<>();
         
         for(int i = 0; i < tangerine.length; i++) {
             hm.put(tangerine[i], hm.getOrDefault(tangerine[i], 0)+1);
         }
         
-        for(int value : hm.values()) {
-            pq.offer(value);
-        }
+        List<Integer> list = new ArrayList<>(hm.values());
+        Collections.sort(list, Collections.reverseOrder());
         
         int sum = 0;
-        while(k > sum) {
-            sum += pq.poll();
+        for(int value : list) {
+            if(k <= sum) break;
+            
+            sum += value;
             answer++;
         }
         
