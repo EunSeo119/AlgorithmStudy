eca52ed7cff6570cc6de2f4dbd26a4a62105c496
EunSeo119 <64001133+EunSeo119@users.noreply.github.com>
2024-11-10 03:25:31 +0900

    [Silver II] Title: 나무 자르기, Time: 468 ms, Memory: 168164 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,40 +1,69 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+/**
+ * 풀이: 이분탐색 활용
+ */
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[] trees = new int[N];
-		int max = 0;
-		for(int i = 0; i < N; i++) {
-			trees[i] = sc.nextInt();
-			if(trees[i]>max) {
-				max = trees[i];
-			}
-		}
-		
-		int high = max;
-		int low = 0;
-		int mid = 0;
-		int result = 0;
-		while(low<=high) {
-			mid = (high+low)/2;
-			long count = 0;
-			for(int i = 0; i < N; i++) {
-				if(trees[i]>=mid) {
-					count+=(trees[i]-mid);
-				}
-			}
-			if(count>=M) {
-				low=mid+1;
-				if(mid>=result) {
-					result = mid;
-				}
-			} else {
-				high=mid-1;
-			}
-		}
-		System.out.println(result);
-	}
+    static int[] tree;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        tree = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            if(tree[i] > max) {
+                max = tree[i];  // 입력값 받을 때 찾을 수 있으면 잘 활용하기
+            }
+        }
+
+        int start = 0;
+        int end = max;
+        int mid = -1;
+        int result = 0;
+        while(start <= end) {   // '=' 꼭 포함 해주기
+            mid = (start + end) / 2;
+            long cutTree = cut(mid);    // 나무 길이 20억이기 때문에 long
+            if(cutTree == M) {
+                result = mid;
+                break;
+            }
+
+            if(cutTree > M) {
+                if(mid > result) {
+                    result = mid;
+                }
+                start = mid+1;  // 둘 다 더하고
+                continue;
+            }
+
+            if(cutTree < M) {
+                end = mid-1;    // 둘 다 빼기
+                continue;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static long cut(int mid) {
+        long cutTree = 0;
+        for(int i = 0; i < N; i++) {
+            if(tree[i] > mid) {
+                cutTree += tree[i] - mid;
+            }
+        }
+
+        return cutTree;
+    }
 }
